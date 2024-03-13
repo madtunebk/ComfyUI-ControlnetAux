a055784f807636dab7c17f0a346da86ccbaba232
madtunebk <128463309+madtunebk@users.noreply.github.com>
2024-03-13 20:37:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description='This ComfyUI custom node, named ControlNet Auxiliar, is designed to provide auxiliary functionalities for image processing tasks. It is particularly useful for various image manipulation and enhancement operations. The node is integrated with functionalities for converting images between different formats and applying various image processing techniques.',
     author='Cornea Valentin',
     author_email='valicornea84@gmail.com',
-    url='https://github.com/madtunebk/ComfyUI_ControlnetAux',
+    url='https://github.com/madtunebk/ComfyUI-ControlnetAux',
     packages=find_packages(),
     install_requires=[
         'timm==0.6.12',
